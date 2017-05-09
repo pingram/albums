f27f0a76181ce2c2caabea0a1eb80273288aeb2e
Philip Ingram <philip.d.ingram@gmail.com>
2017-05-08 20:11:27 -0700

    render basic component

---
+++ index.ios.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Text, AppRegistry } from 'react-native';
+
+// Create a component
+const App = () => (
+  <Text>Some Text</Text>
+);
+
+// Render it to the device
+AppRegistry.registerComponent('albums', () => App);
